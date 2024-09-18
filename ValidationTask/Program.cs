afc2f178940c3372eee2286e42b368c3412d5d94
Kaysan Abdullah <21KAbdullah@heckgrammar.co.uk>
2024-09-18 14:14:46 +0100

    pls

--- ValidationTask/Program.cs
+++ ValidationTask/Program.cs
@@ -4,17 +4,29 @@
     {
         static void Main(string[] args)
         {
-            string firstName, lastName, username, password, emailAddress;
-            int age;
+            string firstName = "", lastName = "", username = "", password = "", emailAddress = "";
+            int age = 0;
 
             // get the user inputs until all are valid.
             // The username should only be output once
-            Console.Write("Enter first name: ");
-            firstName = Console.ReadLine();
-            Console.Write("Enter last name: ");
-            lastName = Console.ReadLine();
-            Console.Write("Enter age: ");
-            age = Convert.ToInt32(Console.ReadLine());
+            while (!ValidName(firstName))
+            {
+
+             Console.Write("Enter first name: ");
+             firstName = Console.ReadLine();
+
+            }
+            while (!ValidName(lastName))
+            {
+                Console.Write("Enter last name: ");
+                lastName = Console.ReadLine();
+            }
+            while (!ValidAge(age))
+            { 
+                Console.Write("Enter age: ");
+                age = Convert.ToInt32(Console.ReadLine());
+                
+            }
             Console.Write("Enter Password: ");
             password = Console.ReadLine();
             Console.Write("Enter email address: ");
@@ -30,12 +42,31 @@
         }
         static bool ValidName(string name)
         {
+            int i = 0;
             // name must be at least two characters and contain only letters
+            for (i = 0; i < name.Length; i++)
+            {
+               string character[i] = name.Substring(i, 1);
+                int code = Convert.ToByte(character[i]);
+                if (code < 65 || code > 121 || (90 < code && code < 97) )
+                {
+                    return false;
+                }
+            }
+            if (name.Length < 2) { return false; }  
+            return true;
         }
 
-        static bool validAge(int age)
+        static bool ValidAge(int age)
         {
             //age must be between 11 and 18 inclusive
+            if (age <= 18 && age >= 11)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
 
         }
 
@@ -43,6 +74,7 @@
         static bool ValidPassword(string password)
         {
             // Check password is at least 8 characters in length
+            if (password.Length < 8) { return false; }
 
 
             // Check password contains a mix of lower case, upper case and non letter characters
@@ -72,13 +104,9 @@
         }
         static string createUserName(string firstName, string lastName, int age)
         {
-            // username is made up from:
-            // first two characters of first name
-            // last two characters of last name
-            // age
-            //e.g. Bob Smith aged 34 would have the username Both34
+            string username = firstName.Substring( 0 , 2) + lastName.Substring( 0 , 2 ) + Convert.ToString(age);
 
-
+            return username;
 
         }
 
