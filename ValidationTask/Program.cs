a8360590f6e8e0a2f3f59a51c380a16d87826937
Kaysan Abdullah <21KAbdullah@heckgrammar.co.uk>
2024-09-30 19:52:27 +0100

    pls save 2 pls

--- ValidationTask/Program.cs
+++ ValidationTask/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Linq;
+﻿using System.Diagnostics.Metrics;
+using System.Xml.Linq;
 
 namespace ValidationTask
 {
@@ -17,15 +18,15 @@ namespace ValidationTask
             {
 
              Console.Write("Enter first name: ");
-             firstName = Console.ReadLine();
+             firstName = Console.ReadLine() ?? "";
 
             }
             Console.Write("Enter last name: ");
-            lastName = Console.ReadLine();
+            lastName = Console.ReadLine() ?? "";
             while (!ValidName(lastName))
             {
                 Console.Write("Enter last name: ");
-                lastName = Console.ReadLine();
+                lastName = Console.ReadLine() ?? "";
             }
             Console.Write("Enter age: ");
                 age = Convert.ToInt32(Console.ReadLine());
@@ -36,21 +37,21 @@ namespace ValidationTask
                 
             }
             Console.Write("Enter Password: ");
-            password = Console.ReadLine();
+            password = Console.ReadLine() ?? "";
             while (!ValidPassword(password))
             {
              Console.Write("Enter Password: ");
-             password = Console.ReadLine();
+             password = Console.ReadLine() ?? "";
             }
             Console.Write("Enter email address: ");
-            emailAddress = Console.ReadLine();
+            emailAddress = Console.ReadLine() ?? "";
             while (!ValidEmail(emailAddress))
             {
                 Console.Write("Enter email address: ");
-                emailAddress = Console.ReadLine();
+                emailAddress = Console.ReadLine() ?? "";
             }
 
-            username = createUserName(firstName,lastName,age);
+            username = CreateUserName(firstName,lastName,age);
             Console.WriteLine($"Username is {username}, you have successfully registered please remember your password");
 
             //  Test your program with a range of tests to show all validation works
@@ -64,13 +65,18 @@ namespace ValidationTask
             for (i = 0; i < name.Length; i++)
             {
                string character = name.Substring(i, 1);
-                int code = Convert.ToByte(character);
+                int code = Convert.ToChar(character);
                 if (code < 65 || code > 122 || (90 < code && code < 97) )
                 {
+                    Console.WriteLine( "Name contains non-letter characters");
                     return false;
                 }
             }
-            if (name.Length < 2) { return false; }  
+            if (name.Length < 2) 
+            {
+                Console.WriteLine(  "Name is too Short");
+                return false; 
+            }  
             return true;
         }
 
@@ -82,6 +88,7 @@ namespace ValidationTask
                 return true;
             } else
             {
+                Console.WriteLine(  "Age is incorrect");
                 return false;
             }
 
@@ -97,7 +104,7 @@ namespace ValidationTask
             if (password.Length < 8) { return false; }
             for (int i = 0; i < password.Length; i++)
             {
-                int code = Convert.ToByte(password.Substring(i, 1));
+                int code = Convert.ToChar(password[i]);
                 if (code >= 65 && code <= 90)
                 {
                     checkUpCase = true;
@@ -113,6 +120,7 @@ namespace ValidationTask
             }
             if (!checkUpCase || !checkLowCase || !checkNonLetter) 
             {
+                Console.WriteLine(" Password does not contain a mix of lower case, upper case and non letter characters");
                 return false;
             }
 
@@ -124,19 +132,21 @@ namespace ValidationTask
 
             for (int i = 0; i < password.Length - 2 ; i++)
             {
-               int a = Convert.ToInt32(Convert.ToByte(password[i]));
-               int b = Convert.ToInt32(Convert.ToByte(password[i + 1]));
-               int c = Convert.ToInt32(Convert.ToByte(password[i + 2]));
+               int a = Convert.ToInt32(Convert.ToChar(password[i]));
+               int b = Convert.ToInt32(Convert.ToChar(password[i + 1]));
+               int c = Convert.ToInt32(Convert.ToChar(password[i + 2]));
                if ((((a >= 65 && a <= 90) || (a >= 97 && a <= 122)) && ((b >= 65 && b <= 90) || (b >= 97 && b <= 122)) && ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)))
                     && ((a + 1 == b && (b + 1 == c || b + 33 == c || b - 31 == c)) || (a + 33 == b && (b + 1 == c || b - 31 == c)) || (a - 31 == b && (b + 33 == c || b + 1 == c)))
                     || ((a - 1 == b && (b - 1 == c || b + 31 == c || b - 33 == c)) || (a + 31 == b && (b - 1 == c || b - 33 == c)) || (a - 33 == b && (b + 31 == c || b - 1 == c)))
                     || ((a == b && (b == c || b + 32 == c || b - 32 == c)) || (a + 32 == b && (b == c || b - 32 == c)) || (a - 32 == b && (b + 32 == c || b == c))))             
                 {
+                    Console.WriteLine("Password contains more than 2 consecutive or repeating letters");
                     return false;
                 }
                
                else if (((a >= 48 && a <= 57) && (b >= 48 && b <= 57) && (c >= 48 && c <= 57)) && ((a + 1 == b && b + 1 == c) || (a - 1 == b && b - 1 == c) || (a == b && b == c)))
                 {
+                    Console.WriteLine("Password contains  more than 2 consecutive or repeating numbers");
                     return false;
                 }
             }
@@ -157,7 +167,7 @@ namespace ValidationTask
             int[] positionDots = new int[email.Length - 3];
             for (int i = 0; i < email.Length; i++)
             {
-                int c = Convert.ToInt32(Convert.ToByte(email[i]));
+                int c = Convert.ToInt32(Convert.ToChar(email[i]));
                 if (c == 64) 
                 {
                     countAt++;
@@ -173,29 +183,35 @@ namespace ValidationTask
                 }
                 if ((c > 32 && c < 65) || c > 122 || (c > 90 && c < 97) && c != 64 && c!= 46)
                 {
+                    Console.WriteLine( "Email can only contain @ . and letters" );
                     return false;
                 }
             }
             if( countAt != 1 || countDot < 1)
             {
+                Console.WriteLine( "More than 1 @ sign is used" );
                 return false;
             }
             if (positionAt < 2 || positionAt > email.Length - 3)
             {
+                Console.WriteLine("Email must have at least 2 characters followed by an @ symbol");
                 return false;
             }
             if ( positionDots[0] < 2 )
             {
+                Console.WriteLine("Email must have at least 2 characters followed by a .");
                 return false;
             }
             for (int i = 0; i < countDot - 1; i++ )
             {
                 if (positionDots[i] - positionAt < 3 && positionDots[i] - positionAt > -3)
                 {
+                    Console.WriteLine("Dots are too close to the @ symbol");
                     return false;
                 }
-                if (positionDots[i + 1] - positionDots[i] < 3 && positionDots[i + 1] - positionDots[i] > -3)
+                if (positionDots[i + 1] - positionDots[i] < 3)
                 {
+                    Console.WriteLine("Dots are too close together");
                     return false;
                 }
             }
@@ -208,7 +224,7 @@ namespace ValidationTask
             // does not contain any other non letter or number characters
 
         }
-        static string createUserName(string firstName, string lastName, int age)
+        static string CreateUserName(string firstName, string lastName, int age)
         {
             string username = firstName.Substring( 0 , 2) + lastName.Substring( 0 , 2 ) + Convert.ToString(age);
 
